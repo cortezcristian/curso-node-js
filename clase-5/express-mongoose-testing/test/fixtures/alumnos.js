ac583d69289e75406a35e95ee9cc14c197ee7df4
Cristian Cortez <cortez.cristian@gmail.com>
2013-10-24 14:22:15 -0300

    Agregado Test Case unitario

---
+++ clase-5/express-mongoose-testing/test/fixtures/alumnos.js
@@ -0,0 +1,13 @@
+//var ObjectId = require('mongoose').Schema.ObjectId;
+
+module.exports.Personas = [{
+    _id    : idAlumno,
+    nombre : "Jose",
+    cargo  : "Alumno"
+},{ 
+    nombre : "Jorge",
+    cargo  : "Alumno"
+},{ 
+    nombre : "Juan Pablo",
+    cargo  : "Alumno"
+}];
--- clase-5/express-mongoose-testing/test/unit/db/personas.js
+++ clase-5/express-mongoose-testing/test/unit/db/personas.js
@@ -1,7 +1,7 @@
 var assert = require("assert")
     , utils = require('../../../utils')
     , fixtures = require('mongoose-fixtures')
-    , Personas;
+    , Personas, idAlumno;
 
 /**
 * Database Connection
@@ -21,22 +21,36 @@ describe('Model Test Personas', function(){
     describe('Operaciones con Alumnos', function(){
         it('Crear un nuevo alumno en la BD', function(done){
             var p = new Personas({nombre: "Jorge", cargo: "Alumno"});
-            p.save(done);
+            p.save(function(err, p){
+                alumnoId = p._id; 
+                done();   
+            });
         });
 
         it('Obtener Datos de Alumno', function(done){
-            var p = new Personas({nombre: "Jorge", cargo: "Alumno"});
-            p.save(done);
+            Personas.obtenerAlumno(alumnoId, function(pers){
+                if(pers[0].nombre == "Jorge"){
+                    done()    
+                }else{
+                    throw new Error('El alumno no fue encontrado');
+                }
+            });
+            //fixtures.load('../../fixtures/alumnos.js', function(err){
+            //});
         });
 
         it('Editar Datos de Alumno', function(done){
-            var p = new Personas({nombre: "Jorge", cargo: "Alumno"});
-            p.save(done);
+            Personas.editarAlumno(alumnoId, "Ramon", function(pers){
+                if(pers.nombre == "Ramon"){
+                    done()    
+                }else{
+                    throw new Error('El alumno no fue encontrado');
+                }
+            });
         });
 
         it('Borrar Alumno', function(done){
-            var p = new Personas({nombre: "Jorge", cargo: "Alumno"});
-            p.save(done);
+              Personas.eliminarAlumno(alumnoId, done);
         });
     });
     
